b83c6b77d09a08b5dbb2e8b651368dd454b40af4
kkien98 <wippykid@gmail.com>
2019-05-14 22:05:48 +0700

    abc haha

---
+++ session1/intro.js
@@ -0,0 +1,11 @@
+// let name= 1;
+// name=5;
+// console.log("Hello",name);
+
+//prompt cho người dùng nhập vào
+
+let x =Number(prompt("Nhập số thứ nhất: "));
+let y =Number(prompt("Nhập số thứ hai: "));
+// a=parseInt(x);
+// b=parseInt(y);
+console.log(x+y);
